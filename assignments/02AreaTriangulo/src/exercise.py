77d7e7562074fe9b748c67e8470581d49c0a2bdc
Sara-SV5822 <88995316+Sara-SV5822@users.noreply.github.com>
2021-08-18 13:57:42 -0500

    Update exercise.py

--- assignments/02AreaTriangulo/src/exercise.py
+++ assignments/02AreaTriangulo/src/exercise.py
@@ -1,5 +1,14 @@
 def main():
     #escribe tu código abajo de esta línea
+  b=float(input('Ingrese el valor de la base: '))
+  h=float(input('Ingrese el valor de la altura: '))
+
+  area=b*h/2
+  mensaje='El área es: {}'
+
+  print(mensaje.format(area))
+
+
     pass
 
 
