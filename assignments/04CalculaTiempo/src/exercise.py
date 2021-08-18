a492a0ccdbcdbfbc7a992e988c8da809fdd79836
Sara-SV5822 <88995316+Sara-SV5822@users.noreply.github.com>
2021-08-18 14:20:53 -0500

    Update exercise.py

--- assignments/04CalculaTiempo/src/exercise.py
+++ assignments/04CalculaTiempo/src/exercise.py
@@ -1,8 +1,11 @@
 def main():
-    #escribe tu código abajo de esta línea
-    pass
+  #escribe tu código abajo de esta línea
+  edad=int(input('Dame tu edad: '))
+  year=int(input('Dame el año actual: '))
 
+  resultado= int((100-edad)+year)
 
+  print('Cumplirás 100 años en el año: '+ str(resultado))
 
 if __name__ == '__main__':
     main()
