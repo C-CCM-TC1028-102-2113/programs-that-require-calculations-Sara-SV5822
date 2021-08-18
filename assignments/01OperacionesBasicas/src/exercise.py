e45788f110718200d5e2b1f938aa677e78fc3db7
Sara-SV5822 <88995316+Sara-SV5822@users.noreply.github.com>
2021-08-18 13:47:29 -0500

    Update exercise.py

--- assignments/01OperacionesBasicas/src/exercise.py
+++ assignments/01OperacionesBasicas/src/exercise.py
@@ -1,5 +1,18 @@
 def main():
     #escribe tu código abajo de esta línea
+  num_1=int(input('Introduce un número: '))
+  num_2=int(input('Introduce un número: '))
+
+  suma=num_1+num_2
+  resta=num_1-num_2
+  mult=num_1*num_2
+
+  mensaje='''Suma= {}
+  Resta= {}
+  Multiplicación= {}'''
+
+  print(mensaje.format(suma,resta,mult))
+    
     pass
 
 
