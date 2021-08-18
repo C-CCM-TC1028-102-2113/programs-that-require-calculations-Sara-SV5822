0c0f049c322407f6bd0388354622d45c258d8ecf
Sara-SV5822 <88995316+Sara-SV5822@users.noreply.github.com>
2021-08-18 14:26:22 -0500

    Update exercise.py

--- assignments/05Lustros/src/exercise.py
+++ assignments/05Lustros/src/exercise.py
@@ -1,9 +1,9 @@
 def main():
   #escribe tu código abajo de esta línea
   born=int(input('Dame el año de nacimiento: '))
-  now=int(input('Dame el año actual'))
+  now=int(input('Dame el año actual: '))
 
-  lustro= (now-born)/5
+  lustro= float( (now-born)/5)
 
   print('Los lustros que has vivido son: '+ str(lustro))
 
