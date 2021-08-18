fb6d4b8d12806b7ea0ea6d7903ebe31e6fa9efe4
Sara-SV5822 <88995316+Sara-SV5822@users.noreply.github.com>
2021-08-18 14:59:36 -0500

    Update exercise.py

--- assignments/06Peso/src/exercise.py
+++ assignments/06Peso/src/exercise.py
@@ -1,8 +1,12 @@
 def main():
-    #escribe tu código abajo de esta línea
-    pass
-    
+  #escribe tu código abajo de esta línea
+  w_i=float(input('Dame el peso inicial: '))
+  w_f=float(input('Dame el peso final: '))
+  mes=int(input('Dame la cantidad de meses: '))
 
+  w_m=float((w_i-w_f)/mes)
+
+  print('Lo que debes bajar por mes es: '+ str(w_m))
 
 if __name__ == '__main__':
     main()
