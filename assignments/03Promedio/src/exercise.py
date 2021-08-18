d530b53ea4d3a3cf5ca4b79fbf07704e0721c54f
Sara-SV5822 <88995316+Sara-SV5822@users.noreply.github.com>
2021-08-18 14:16:00 -0500

    Update exercise.py

--- assignments/03Promedio/src/exercise.py
+++ assignments/03Promedio/src/exercise.py
@@ -1,7 +1,13 @@
 def main():
-    #escribe tu código abajo de esta línea
-    pass
+  #escribe tu código abajo de esta línea
+  cal_1=float(input('Calificación de la materia: '))
+  cal_2=float(input('Calificación de la materia: '))
+  cal_3=float(input('Calificación de la materia: '))
+  cal_4=float(input('Calificación de la materia: '))
 
+  prom=(cal_1+cal_2+cal_3+cal_4)/4
+
+  print('El promedio es: '+ str(prom))
 
 if __name__ == '__main__':
     main()
