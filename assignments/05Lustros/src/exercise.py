10799f068b57952651fd21ff35e4612f479a58e2
Sara-SV5822 <88995316+Sara-SV5822@users.noreply.github.com>
2021-08-18 14:23:29 -0500

    Update exercise.py

--- assignments/05Lustros/src/exercise.py
+++ assignments/05Lustros/src/exercise.py
@@ -1,8 +1,11 @@
 def main():
-    #escribe tu código abajo de esta línea
-    pass
+  #escribe tu código abajo de esta línea
+  born=int(input('Dame el año de nacimiento: '))
+  now=int(input('Dame el año actual'))
 
+  lustro= (now-born)/5
 
+  print('Los lustros que has vivido son: '+ str(lustro))
 
 if __name__ == '__main__':
     main()
